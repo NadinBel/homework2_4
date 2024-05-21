26c57be26115f16c04a93380fa062b42944b5e89
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-05-21 17:59:57 +0300

    homework2_4.py

--- homework2_4.py
+++ homework2_4.py
@@ -1,7 +1,7 @@
 car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-car_counts = 0
 for i in car:
     print('Я езжу на автомабиле марки', i)
+    car_counts = 0
     for i in range(len(car)):
         car_counts += 10
         print(car_counts)
